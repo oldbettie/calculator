ed2bfa628664a7e1c2d54934a7e46f42f80b00e7
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-03-23 11:03:05 +0800

    first logic

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log("hello");
+const $screen = $(".screen");
+const $numbers = $(".numb");
+const $clear = $("#clear");
+const $percent = $("#percent");
+const $divide = $("#divide");
+const $times = $("#times");
+const $minus = $("#minus");
+const $plus = $("#plus");
+const $equals = $("#equals");
+const $btn = $(".button");
+
+$btn.click(function () {
+	if (!$btn.prop("#clear")) {
+		console.log($btn.attr("#"));
+		$(".screen").append(this.innerHTML);
+	}
+});
+
+//screen = screen + this.text
