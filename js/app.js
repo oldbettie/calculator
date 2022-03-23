130e4e077bd6cca459cb0e39636e5fccb7d4eeb4
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-03-24 06:20:20 +0800

    final calculator v1.0

--- js/app.js
+++ js/app.js
@@ -33,25 +33,20 @@ $clear.click(function () {
 	numb = 0;
 	$screen.removeClass("screen-lol");
 });
-// --- clears the screen for second number input
-const clearScreen = function () {
-	$screen.text("");
-};
 // --- numb = string of inputs before math
 $numbers.click(function () {
 	input = $(this).prop("id");
 	numb += input;
 	console.log(numb);
 });
-
 // --- percent of whole number
 $percent.click(function () {
 	if (numbers.numb1 < 1) {
 		numbers.numb1 = parseFloat(numb);
 	}
 	numb = 0;
-	$screen.text(`% ${numbers.numb1 / 100}`);
 	numbers.numb1 = numbers.numb1 / 100;
+	$screen.text(`% ${numbers.numb1}`);
 	numbers.math = "%";
 });
 // --- divide
@@ -81,7 +76,6 @@ $plus.click(function () {
 	$screen.text("+");
 	numbers.math = "plus";
 	if (numbers.numb1 === 80085) {
-		console.log("event");
 		$screen.addClass("screen-lol");
 	}
 });
@@ -98,33 +92,30 @@ $minus.click(function () {
 $random.click(function () {
 	numbers.numb1 = parseFloat(numb);
 	randomNumber = Math.floor(Math.random() * numbers.numb1);
-	$screen.text(`random 0 to ${numbers.numb1} = ${randomNumber}`);
-	numbers.numb1 = 0;
-	numbers.numb2 = 0;
+	$screen.text(`0 - ${numbers.numb1} = ${randomNumber}`);
 });
 // --- math logic
 $equals.click(function () {
 	numbers.numb2 = parseFloat(numb);
 	if (numbers.math === "divide") {
-		$screen.text(`${numbers.numb1 / numbers.numb2} =`);
 		numbers.numb1 = numbers.numb1 / numbers.numb2;
+		$screen.text(`${numbers.numb1} =`);
 		$allInputs.append(numbers.numb1);
 		numbers.numb2 = 0;
 	} else if (numbers.math === "times") {
-		$screen.text(`${numbers.numb1 * numbers.numb2} =`);
 		numbers.numb1 = numbers.numb1 * numbers.numb2;
+		$screen.text(`${numbers.numb1} =`);
 		$allInputs.append(numbers.numb1);
 		numbers.numb2 = 0;
 	} else if (numbers.math === "plus") {
-		$screen.text(`${numbers.numb1 + numbers.numb2} =`);
 		numbers.numb1 = numbers.numb1 + numbers.numb2;
+		$screen.text(`${numbers.numb1} =`);
 		$allInputs.append(numbers.numb1);
 		numbers.numb2 = 0;
 	} else if (numbers.math === "minus") {
-		$screen.text(`${numbers.numb1 - numbers.numb2} =`);
 		numbers.numb1 = numbers.numb1 - numbers.numb2;
+		$screen.text(`${numbers.numb1} =`);
 		$allInputs.append(numbers.numb1);
 		numbers.numb2 = 0;
 	}
-	// numbers.math = "";
 });
